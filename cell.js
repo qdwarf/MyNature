7e1d8a3751acf210b0f6cd0d352ad66a91b84c66
qdwarf <93176779+qdwarf@users.noreply.github.com>
2021-12-12 03:39:13 +0900

    pos3추가/움직임변경/디스플레이 추가
    전반적으로 코드를 최종 모양새에 가깝게 바꿨다.

--- cell.js
+++ cell.js
@@ -1,55 +1,105 @@
 class Cell {
   constructor() {
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(0,0);
-    this.acc = createVector(0,0);
-    this.speed = 0.5;
-    this.s = random(16, 22);
-    this.r = this.s/2;
+    this.pos1 = createVector(random(width), random(height));
+    this.pos2 = createVector(random(width), random(height));
+    this.pos3 = createVector(random(width), random(height));
+    this.vel = createVector();
+    this.acc = createVector();
+    this.topSpeed = 1.2;
+    this.s = random(10, 25);
+    this.r = this.s / 2;
     this.isThem = false;
   }
 
   update() {
     let person = createVector(mouseX, mouseY);
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
     this.acc = p5.Vector.random2D();
     this.acc.setMag(0.1);
 
     if (this.isThem) {
-      this.vel.add(this.pos, person);
+      this.acc = p5.Vector.sub(this.pos1, person);
+      this.acc = p5.Vector.sub(this.pos2, person);
+      this.acc = p5.Vector.sub(this.pos3, person);
     }
 
     this.vel.add(this.acc);
-    this.vel.mult(this.speed);
-    this.pos.add(this.vel);
+    this.vel.limit(this.topSpeed);
+    this.pos1.add(this.vel);
+    this.pos2.add(this.vel);
+    this.pos3.add(this.vel);
   }
 
   display() {
-    fill(255);
-    stroke(20);
-    ellipse(this.pos.x, this.pos.y, this.s, this.s);
+    fill(20, 225, 70);
+    noStroke();
+
+    ellipse(this.pos1.x, this.pos1.y, this.s, this.s);
+    ellipse(this.pos2.x, this.pos2.y, this.s, this.s);
+    ellipse(this.pos3.x, this.pos3.y, this.s, this.s);
+    stroke(255, 10);
+    line(this.pos1.x, this.pos1.y, this.pos2.x, this.pos2.y);
+    line(this.pos1.x, this.pos1.y, this.pos3.x, this.pos3.y);
+    line(this.pos2.x, this.pos2.y, this.pos3.x, this.pos3.y);
   }
 
   checkEdges() {
-    if (this.pos.y > height - this.r) {
+    if (this.pos1.y > height - this.r) {
       this.vel.y *= -1;
-      this.pos.y = height - this.r;
+      this.pos1.y = height - this.r;
     }
 
-    if (this.pos.y < this.r) {
+    if (this.pos1.y < this.r) {
       this.vel.y *= -1;
-      this.pos.y = this.r;
+      this.pos1.y = this.r;
     }
 
-    if (this.pos.x > width - this.r) {
+    if (this.pos1.x > width - this.r) {
       this.vel.x *= -1;
-      this.pos.x = width - this.r;
+      this.pos1.x = width - this.r;
     }
 
-    if (this.pos.x < this.r) {
+    if (this.pos1.x < this.r) {
       this.vel.x *= -1;
-      this.pos.x = this.r;
+      this.pos1.x = this.r;
+    }
+    if (this.pos2.y > height - this.r) {
+      this.vel.y *= -1;
+      this.pos2.y = height - this.r;
+    }
+
+    if (this.pos2.y < this.r) {
+      this.vel.y *= -1;
+      this.pos2.y = this.r;
+    }
+
+    if (this.pos2.x > width - this.r) {
+      this.vel.x *= -1;
+      this.pos2.x = width - this.r;
+    }
+
+    if (this.pos2.x < this.r) {
+      this.vel.x *= -1;
+      this.pos2.x = this.r;
+    }
+
+    if (this.pos3.y > height - this.r) {
+      this.vel.y *= -1;
+      this.pos3.y = height - this.r;
+    }
+
+    if (this.pos3.y < this.r) {
+      this.vel.y *= -1;
+      this.pos3.y = this.r;
+    }
+
+    if (this.pos3.x > width - this.r) {
+      this.vel.x *= -1;
+      this.pos3.x = width - this.r;
+    }
+
+    if (this.pos3.x < this.r) {
+      this.vel.x *= -1;
+      this.pos3.x = this.r;
     }
   }
 
